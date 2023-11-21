bd40df482b4f56f445e67bae43e0ccde4620cde2
Aniket Satapathy <aniketsatapathy193@gmail.com>
2023-11-22 00:43:42 +0530

    [Backend]Bugs Fixed

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,21 +1,94 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcryptjs');
 const User = require('../models/userModel');
 
 
 const getUserProfile = asyncHandler(async (req, res) => {
-    const { username } = req.params;
+    // Get the user id from the request object
+    const userId = req.user._id;
 
-    // Check if user exists
-    const userExists = await User.findOne({ username }).select('-password');
-    if (!userExists) {
+    // Find the user in the database by id
+    const user = await User.findById(userId).select('-password');
+    if (!user) {
         res.status(404);
-        throw new Error('User does not exist');
+        throw new Error('User not found');
     }
 
+    // Send the user object as response
     res.status(200).json({
-        message: "User profile retrieved",
-        data: {
-            user: userExists
+        message: 'User found',
+        user,
+    });
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const { firstName, lastName, email } = req.body;
+
+    // Get the user id from the request object
+    const userId = req.user._id;
+
+    // Find the user in the database by id
+    const user = await User.findById(userId);
+    if (!user) {
+        res.status(404);
+        throw new Error('User not found');
+    }
+
+    // Update the user object
+    user.firstName = firstName || user.firstName;
+    user.lastName = lastName || user.lastName;
+    user.email = email || user.email;
+
+    // Save the updated user object
+    const updatedUser = await user.save();
+
+    // Send the updated user object as response
+    res.status(200).json({
+        message: 'User updated',
+        user: {
+            _id: updatedUser._id,
+            firstName: updatedUser.firstName,
+            lastName: updatedUser.lastName,
+            email: updatedUser.email,
+        }
+    });
+});
+
+const updatePassword = asyncHandler(async (req, res) => {
+   const { currentPassword, newPassword } = req.body;
+
+    // Get the user id from the request object
+    const userId = req.user._id;
+
+    // Find the user in the database by id
+    const user = await User.findById(userId);
+
+    // Check if the password matches
+    const isMatch = await bcrypt.compare(currentPassword, user.password);
+
+    // If password does not match, throw an error
+    if (!isMatch) {
+        res.status(401);
+        throw new Error('Invalid password');
+    }
+
+    // Hash the new password
+    const salt = await bcrypt.genSalt(10);
+
+    // Update the user object
+    user.password = await bcrypt.hash(newPassword, salt);
+
+    // Save the updated user object
+    const updatedUser = await user.save();
+
+    // Send the updated user object as response
+    res.status(200).json({
+        message: 'Password updated',
+        user: {
+            _id: updatedUser._id,
+            firstName: updatedUser.firstName,
+            lastName: updatedUser.lastName,
+            email: updatedUser.email,
         }
     });
 });
@@ -23,4 +96,6 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 module.exports = {
     getUserProfile,
+    updateUserProfile,
+    updatePassword,
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
-
-const router = express.Router();
-
 const {
     getUserProfile,
+    updateUserProfile,
+    updatePassword,
 } = require('../controllers/userControllers');
 const { protect } = require('../middlewares/authMiddleware');
 
+
+const router = express.Router();
 
 // @desc    Get user profile
 // @route   GET /api/users/profile
@@ -14,5 +15,14 @@ const { protect } = require('../middlewares/authMiddleware');
 router.get('/profile', protect, getUserProfile);
 
 // @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+router.put('/profile', protect, updateUserProfile);
+
+// @desc    Update user password
+// @route   PUT /api/users/password
+// @access  Private
+router.put('/password', protect, updatePassword);
+
 
 module.exports = router;
