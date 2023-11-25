d99216a4b8c2aae5beb1f635a7b57a12d4ebc40f
Aniket Satapathy <aniketsatapathy193@gmail.com>
2023-11-25 22:21:52 +0530

    [Backend]Update

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,10 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
